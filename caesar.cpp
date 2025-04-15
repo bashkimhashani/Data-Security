e68dadb4bfb55e13fb3814b3ab3b6af02a60978b
bashkimhashani <bashkim.hashani@student.uni-pr.edu>
2025-04-15 21:13:04 +0200

    funksioni lexuTekstin

--- caesar.cpp
+++ caesar.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
+#include <fstream>
+
+using namespace std;
+
+string lexoTekstin(const string& filename) {
+    ifstream file(filename);
+
+    if(!file) {
+        cerr << "Nuk mund te hapet fajlli: " << filename << endl;
+        return "";
+    }
+
+    string text, line;
+    while(getline(file, line)) {
+        for(char ch : line){
+            text += tolower(ch);
+        }
+    }
+    return text;
+}
+
 
 int main(){
-    return 0;
 
+    return 0;
 }
